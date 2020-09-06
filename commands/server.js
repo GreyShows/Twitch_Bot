a5d1abba095b29c51f1b863d55358901f7c2adeb
GreyShows <63278260+GreyShows@users.noreply.github.com>
2020-09-05 21:51:31 -0500

    Testing
    Test

--- commands/server.js
+++ commands/server.js
@@ -1,3 +1,13 @@
 exports.run = (client, message, args, user, channel, self) => {
-   client.say(channel, `@${user.username}, The Server IP is`) 
+
+   var noip = "Sorry no IP im not playing a game that im doing multiplayer with viewers"
+   var ip = "The Server ip is "
+
+   if (noip) {
+   client.say(channel, `@${user.username}, ` + noip)
+}
+   if(ip) {
+   client.say(channel, `@${user.username}, ` + ip) 
+}
+
 }
