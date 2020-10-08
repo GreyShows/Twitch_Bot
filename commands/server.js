0c140fdd5c4674605b435447682f921ac8800995
GreyShows <63278260+GreyShows@users.noreply.github.com>
2020-10-07 19:07:14 -0500

    Update server.js

--- commands/server.js
+++ commands/server.js
@@ -1,13 +1,4 @@
 exports.run = (client, message, args, user, channel, self) => {
-
-   var noip = "Sorry no IP im not playing a game that im doing multiplayer with viewers"
-   var ip = "The Server ip is "
-
-   if (noip) {
-   client.say(channel, `@${user.username}, ` + noip)
-}
-   if(ip) {
-   client.say(channel, `@${user.username}, ` + ip) 
-}
-
+   var serverip = "54.219.242.215:12992"
+   client.say(channel, `@${user.username}, The Server IP is ` + serverip)
 }
